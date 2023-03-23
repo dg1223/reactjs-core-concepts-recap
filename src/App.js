afdda61bdea3c72fa48c793196f6935334801b85
dg1223 <alavi1223@hotmail.com>
2023-03-23 00:30:52 -0400

    similar in look, different in data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name="Noakhali" special="bibhag"></District>
+      <District name="Brahmonbaria" special="jodha akbar"></District>
+      <District name="Shumilla" special="Moyna and Moti"></District>
+    </div>
+  );
+}
+
+const districtStyle = {
+  backgroundColor: "yellow",
+  margin: "20px",
+  borderRadius: "20px",
+  border: "3px solid red",
+};
+
+function District(props) {
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.special}</p>
     </div>
   );
 }

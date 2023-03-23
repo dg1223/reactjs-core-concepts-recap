b760068b42e25c486fc3d29626c337bf7737eb6b
dg1223 <alavi1223@hotmail.com>
2023-03-23 01:17:37 -0400

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District name="Noakhali" special="bibhag"></District>
       <District name="Brahmonbaria" special="jodha akbar"></District>
       <District name="Shumilla" special="Moyna and Moti"></District>
+    </div>
+  );
+}
+
+function LoadPosts() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h1>Posts: {posts.length}</h1>
+      {posts.map((post) => (
+        <Post title={post.title} body={post.body}></Post>
+      ))}
+    </div>
+  );
+}
+
+function Post(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: "lightsalmon",
+        margin: "20px",
+        border: "2px solid red",
+        borderRadius: "20px",
+      }}
+    >
+      <h2>Title: {props.title}</h2>
+      <p>Body: {props.body}</p>
     </div>
   );
 }
